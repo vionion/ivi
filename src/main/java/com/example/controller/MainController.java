4a938a006de0a6daad8eda40d8b5116d2f6d76ea
Vitalii Tsybulko <tsybulko.vitalii@gmail.com>
2017-11-26 05:03:50 +0200

    Initial heroku commit.

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,26 +1,13 @@
 package com.example.controller;
 
 
-import org.apache.commons.compress.utils.IOUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import static com.example.utils.IOUtility.getByteArrayFromInputStream;
 
 @Controller
 @RequestMapping(path = "/")
 public class MainController {
-
-    public static final String UPLOAD_AUDIO_LINK = "https://mirror-chat.mybluemix.net/api/uploadAudio";
-    private byte[] response;
 
     /**
      * Method to get index page
@@ -30,47 +17,5 @@ public class MainController {
         return "index";
     }
 
-    /**
-     * Method to upload user's speech to STT
-     *
-     * @param wav
-     *            bytearray of wav with user's speech
-     */
-    @RequestMapping(path = "upload", method = RequestMethod.POST)
-    public @ResponseBody void sendWAV(@RequestBody byte[] wav) throws IOException {
-        URL url = new URL(UPLOAD_AUDIO_LINK);
-        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-        httpConn.setUseCaches(false);
-        httpConn.setDoOutput(true); // indicates POST method
-        httpConn.setDoInput(true);
-
-        httpConn.setRequestMethod("POST");
-        httpConn.setRequestProperty("Connection", "Keep-Alive");
-        httpConn.setRequestProperty("Content-Type", "audio/wav");
-        DataOutputStream request = new DataOutputStream(httpConn.getOutputStream());
-        request.write(wav);
-        request.flush();
-        request.close();
-        int status = httpConn.getResponseCode();
-        if (status == HttpURLConnection.HTTP_OK) {
-            InputStream responseStream = new BufferedInputStream(httpConn.getInputStream());
-            // saving bytearray of response-wav
-            response = getByteArrayFromInputStream(responseStream);
-        }
-        // TODO: add exception handling
-    }
-
-
-    /**
-     * Method to return bot's answer to UI on demand
-     *
-     */
-    @RequestMapping(path = "getResponseAudio", method = RequestMethod.GET)
-    public void getTranslationAudio(HttpServletResponse servletResponse) throws IOException {
-        // I know that it is not the best implementation, obviously. I just wasn't able to make js
-        // playing wav from bytes properly. I believe that with some extra time I would be able to do this.
-        IOUtils.copy(new ByteArrayInputStream(response), servletResponse.getOutputStream());
-        servletResponse.flushBuffer();
-    }
 
 }
